92e07bd2de588b3591ad2e90d45f140dbc914236
Martyn Ling <mling@str8-4ward.com>
2018-03-05 15:13:58 +0000

    Fix dev environment

--- karma.conf.js
+++ karma.conf.js
@@ -3,10 +3,10 @@ module.exports = function (config) {
   config.set({
     browsers: ['PhantomJS'],
     frameworks: ['browserify', 'jasmine'],
-    files: ['test/unit/**/*.js'],
+    files: ['test/unit/:*+/*.js'],
     reporters: ['spec'],
     preprocessors: {
-      'test/unit/**/*.js': ['browserify']
+      'test/unit/:*+/*.js': ['browserify']
     },
     browserify: {
       debug: true,
